5ba9e8f5d5944a9720df8f223d2a9c2bb9b20ef0
Faith_Wasonga <faithwasonga@gmail.com>
2022-12-04 09:37:26 -0800

    Finding the biggest integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return "None"
-    max = my_list[0]
-    for i in range(len(my_list)):
-        if my_list[i] > max:
-            max = my_list[i]
-            return max
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for j in range(len(i)):
+            if j == (len(i) - 1):
+                print("{}".format(i[j]), end="")
+            else:
+                print("{} ".format(i[j]), end="")
+                print()
