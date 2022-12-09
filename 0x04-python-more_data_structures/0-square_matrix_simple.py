e5b7c33c8e1baf4c05b766c47434a75bd3c69704
Faith_Wasonga <faithwasonga@gmail.com>
2022-12-09 07:34:00 -0800

    Computing square value

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
-
-    for i in range(len(matrix)):
-        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
-
-        return (new_matrix)
+    """
+    function thhat return square of all matrix values
+    """
+    if len(matrix) != 0:
+        copy = matrix[:]
+        new_list = []
+        for nlist in matrix:
+            copy = list(map(lambda x: x * x, nlist))
+            new_list.append(copy)
+            return new_list
+        # return list(map(lambda x: list(map(lambda x: x * x, x)), matrix))
