ca75be2998cfd1e6318e9654b0368e78ffdf2876
Faith_Wasonga <faithwasonga@gmail.com>
2023-03-17 04:12:30 -0700

    Creating non-existing database

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- Creates the database hbtn_0c_0 in my MySQL server.
 -- use exit code 0 if hbtn_0c_0 already exists.
-CREATE DATABASE IF NOT EXISTS 'hbtn_0c_0'
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
